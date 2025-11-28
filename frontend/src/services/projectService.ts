f97e8291c0d68dac401e240daf5c604d14fdec93
muhammadajmalvn <muhammadajmalvn@gmail.com>
2025-11-28 11:15:07 +0530

    feat/frontend: api integration

--- frontend/src/services/projectService.ts
+++ frontend/src/services/projectService.ts
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { privateClient } from "../utils/privateClient";
 
 
 
-export async function createNewProject(): Promise<unknown> {
+export async function createNewProject(): Promise<any> {
   try {
     const response = await privateClient.get("/project/new");
     const { data, status } = response;
