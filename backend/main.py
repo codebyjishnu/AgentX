166a985a86e5c6a5072d7defd2939e62e427e55c
muhammadajmalvn <muhammadajmalvn@gmail.com>
2025-12-01 17:56:31 +0530

    feat/backend: added cors middleware

--- backend/main.py
+++ backend/main.py
@@ -12,5 +12,7 @@ app = FastAPI()
 async def root():
     return {"message": "Success"}
 
+app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_methods=["*"],allow_headers=["*"])
+
 app.include_router(router)
     
